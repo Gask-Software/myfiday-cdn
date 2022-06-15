0eb20ae51600b419caf90c1e6c4e9ef72bd0c115
Adam Gask <ajgask@gmail.com>
2022-06-15 10:22:43 +0100

    Manual header add

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ var corsOptions = {
 
 app.use(cors(corsOptions))
 
+
+app.all('/', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
