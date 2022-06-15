b2dcff9eba6cc59eec5cd887a0c5bc88b1da8ea7
Adam Gask <ajgask@gmail.com>
2022-06-15 10:26:36 +0100

    remove slash

--- app.js
+++ app.js
@@ -29,12 +29,6 @@ var corsOptions = {
 app.use(cors(corsOptions))
 
 
-app.all('/', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
